5874931fc8c08a7d701f72caa0f88c0e4c7acfbd
GAH97-B\Aris <aris.ppd@gmail.com>
2018-02-05 18:29:11 +0200

    Mobile version

--- js/app.js
+++ js/app.js
@@ -17,8 +17,7 @@ for (i=0; i<5; i++) {
         y--;
         m = 11;
     }
-    months_array.unshift(months[m] + ' ' + y + ' ' + (m+1) + ' ' + monthsShort[m]); // Passes 1) Month as a text, 2) Year, 3) Month as a readable number (+1) 
-    console.log(months_array);
+    months_array.unshift(months[m] + ' ' + y + ' ' + (m+1) + ' ' + monthsShort[m]); // Passes 1) Month as a text, 2) Year, 3) Month as a readable number (+1)
 }
 
 // Get 11 next months
@@ -29,43 +28,55 @@ for (i=0; i<11; i++) {
         mm = 0;
     }
     months_array.push(months[mm] + ' ' + yy + ' ' + (mm+1) + ' ' + monthsShort[mm]);
-    console.log(months_array);
 }
 
 months_array.forEach(function(el, index) {
-    console.log('element', el);
 var split_el = el.split(" ");
 var dateText = "<span class='fullMonth'>" + split_el[0] + "</span>" + "<span class='shortMonth'>" + split_el[3] + "</span>" + "<br>" + split_el[1];
 
 // console.log('foreach', dateText, index);
-if (index < 6) {
-    document.getElementById('app').innerHTML += "<div class='month col m1 active' data-month='" + split_el[2] + "' data-year='" + split_el[1] + "'><p class='dateText'>" + dateText + "</p></div>";
+if (index < 5) {
+    document.getElementById('app').innerHTML += "<div class='month col s12 m1 show' data-month='" + split_el[2] + "' data-year='" + split_el[1] + "'><p class='dateText'>" + dateText + "</p></div>";
+    document.getElementById('agenda-dropdown').innerHTML += "<li data-month='" + split_el[2] + "' data-year='" + split_el[1] + "'><a href='#'>" + split_el[0] + ' ' + split_el[1] + "</a></li>";
+
+}
+else if (index == 5) {
+    document.getElementById('app').innerHTML += "<div class='month col s12 m1 show active' data-month='" + split_el[2] + "' data-year='" + split_el[1] + "'><p class='dateText'>" + dateText + "</p></div>";
+    document.getElementById('agenda-dropdown').innerHTML += "<li class='divider'></li>" + "<li data-month='" + split_el[2] + "' data-year='" + split_el[1] + "' class='active'><a href='#'>" + split_el[0] + ' ' + split_el[1] + "</a></li>";
 }
 else if (index < 12) {
-    document.getElementById('app').innerHTML += "<div class='month col m1 active second-half' data-month='" + split_el[2] + "' data-year='" + split_el[1] + "'><p class='dateText'>" + dateText + "</p></div>";
+    document.getElementById('app').innerHTML += "<div class='month col s12 m1 show second-half' data-month='" + split_el[2] + "' data-year='" + split_el[1] + "'><p class='dateText'>" + dateText + "</p></div>";
+    document.getElementById('agenda-dropdown').innerHTML += "<li data-month='" + split_el[2] + "' data-year='" + split_el[1] + "'><a href='#'>" + split_el[0] + ' ' + split_el[1] + "</a></li>";
 }
 else {
-    document.getElementById('app').innerHTML += "<div class='month col m1' data-month='" + split_el[2] + "' data-year='" + split_el[1] + "'><p class='dateText'>" + dateText + "</p></div>";
+    document.getElementById('app').innerHTML += "<div class='month col s12 m1' data-month='" + split_el[2] + "' data-year='" + split_el[1] + "'><p class='dateText'>" + dateText + "</p></div>";
+    document.getElementById('agenda-dropdown').innerHTML += "<li data-month='" + split_el[2] + "' data-year='" + split_el[1] + "'><a href='#'>" + split_el[0] + ' ' + split_el[1] + "</a></li>";
 }
+// Class "show" refers to whether the div is shown on tablets and up, class "active" refers to active month on mobile version
+
 // Appending HTML, with classes according to initial state.
 });
 
 
 // Calculate agenda widths
-var agenda_w = $('.agenda').outerWidth();
+var agenda_w = $('#agenda').outerWidth();
+var documentWidth = window.innerWidth;
 
-//var month_w = agenda_w / 12;
+// var month_w = agenda_w / 12;
 // $('.month').outerWidth(month_w); // divided by 12 months, minus 2 pixels (not calculating border)
 // $('#app').outerWidth(month_w * 17);
 /* Old method: Individual month divs are calculated first, then we multiply their width by 17 (total months). 
     This method turned out to be less effective cause browsers render divisions of pixel in a different way (read about subpixel rendering),
     causing a loss of +-2 pixels in some cases */
-$('#app').outerWidth(agenda_w * 1.4166666); 
+if (documentWidth > 600) {
+    $('#app').outerWidth(agenda_w * 1.4166666);
+}  
 /* New method: Get agenda width when page is loaded, and set the width to be 17/12=1.4166666 larger (total months divided by months shown)
     Month wrapper width is calculated from the outer div width, in order to be able to use a fixed percentage width with CSS in the month divs,
     thus letting the browsers render properly and in their own way the pixel divisions. */
 
 
+// Tablet and up click on arrows event
 $('.agenda-btn').on('click', function(e) {
 
     e.preventDefault();
@@ -83,32 +94,50 @@ $('.agenda-btn').on('click', function(e) {
         toggleActiveDivs();
         setTimeout(() => {
         toggleSecondHalfDivs();
-    }, 50);
-}
+        }, 50);
+    }
 
-function toggleActiveDivs() {
-    $('.month').each(function(index, el) {
+    function toggleActiveDivs() {
+        $('.month').each(function(index, el) {
         if (index < 5) {
-            $(this).toggleClass('active');
+            $(this).toggleClass('show');
         }
         else if (index > 11) {
-            $(this).toggleClass('active');
+            $(this).toggleClass('show');
         }
-    });
-}
-function toggleSecondHalfDivs() {
-    $('.month.active').each(function(index, el) {
+        });
+    }
+    function toggleSecondHalfDivs() {
+        $('.month.show').each(function(index, el) {
         if (index <= 5) {
             $(this).removeClass('second-half');
         }
         else {
             $(this).addClass('second-half');
         }
-    });
-}
+        });
+    }
+        
+});
+
+// Mobile dropdown functionality
+$('#agenda-dropdown li').on('click', function(e) {
+
+    e.preventDefault();
+
+    $('#agenda-dropdown, #agenda-dropdown li').removeClass('active');
+    $(this).addClass('active');
+    var selectedMonth = $(this).data('month');
+    var selectedYear = $(this).data('year');
+    $('.month').removeClass('active');
+    $('.month[data-month="' + selectedMonth + '"][data-year="' + selectedYear + '"]').addClass('active');
+
 
 });
 
+$('.dropdown-button.btn').on('click', function(){
+    $('#agenda-dropdown').toggleClass('active');
+});
 
 
 getJSON.events.forEach(function(event){
@@ -128,26 +157,27 @@ getJSON.events.forEach(function(event){
     eventYears = [parseInt(startingYear)];
 
     while (startingMonth != endingMonth) {
-    startingMonth++;
-    if (startingMonth > 12) {
+        startingMonth++;
+        if (startingMonth > 12) {
         startingMonth = 1;
         startingYear++;
-    }
-    eventMonths.push(parseInt(startingMonth));
-    eventYears.push(parseInt(startingYear));
-    }
+        }
+        eventMonths.push(parseInt(startingMonth));
+        eventYears.push(parseInt(startingYear));
+    } // If event lasts more than a month, create arrays that show which month and year the event take place in 
     // console.log('eventMonths', eventMonths);
     // console.log('eventYears', eventYears);
 
     for (i=0; i<eventMonths.length; i++) {
-    // console.log('eventMonths[i]',eventMonths[i], $('.month[data-month="' + eventMonths[i] + '"]'));
-    $('.month[data-month="' + eventMonths[i] + '"]').append("\
-    <div class='event'>\
-        <p class='event-title'>" + event.title + "</p>\
-        <p class='event-location'>Location: " + event.location + "</p>\
-        <p class='event-type'>Type: " + event.type + "</p>\
-        <p class='event-period'>From " + event.starting + " to " + event.ending + "</p>\
-    </div>");
-    }
+        // console.log('eventMonths[i]',eventMonths[i], $('.month[data-month="' + eventMonths[i] + '"]'));
+        $('.month[data-month="' + eventMonths[i] + '"][data-year="' + eventYears[i] + '"]').append("\
+        <div class='event'>\
+            <p class='event-title'>" + event.title + "</p>\
+            <p class='event-location'>Location : " + event.location + "</p>\
+            <p class='event-type'>Type : " + event.type + "</p>\
+            <p class='event-period'>Period : From " + event.starting + " to " + event.ending + "</p>\
+        </div>");
+    } // Append current event in the div 
+
 
 });
